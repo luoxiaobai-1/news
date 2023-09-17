16ed24c0a2d8856aa6e1acc21d094187c3d505c0
luoxiaobai-1 <2690784730@qq.com>
2023-09-17 21:38:29 +0800

    FIRST COMMIT

---
+++ heima-leadnews-service/leaduser-user/src/main/java/com/me/user/userApplication.java
@@ -0,0 +1,17 @@
+package com.me.user;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.me.user.mapper")
+public class userApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(userApplication.class);
+
+    }
+}
